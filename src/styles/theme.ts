b03678bfd4018c5f9f81e24a031cc48e489d1d96
Lucas Felix <lucariozin.net@gmail.com>
2023-01-23 18:33:32 -0300

    configuring the color scheme.

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -10,6 +10,27 @@ const sizesObj: { [K in Sizes]: number } = {
 export const theme = {
   palette: {
     white: '#FFFFFF',
+    yellow: {
+      100: '#F1E9C9',
+      400: '#DBAC2C',
+      500: '#C47F17',
+    },
+    purple: {
+      100: '#EBE5F9',
+      400: '#8047F8',
+      500: '#4B2995',
+    },
+    gray: {
+      100: '#FAFAFA',
+      200: '#F3F2F2',
+      300: '#EDEDED',
+      400: '#E6E5E5',
+      500: '#D7D5D5',
+      600: '#8D8686',
+      700: '#574F4D',
+      800: '#403937',
+      900: '#272221',
+    },
   },
   breakpoints: {
     sizes: sizesObj,
