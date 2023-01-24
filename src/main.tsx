016e37594ec76ee59200ddbe0d7ee59bd114feca
Lucas Felix <lucariozin.net@gmail.com>
2023-01-24 11:57:45 -0300

    configuring ts paths.

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 
 import { ThemeProvider } from 'styled-components'
 
-import { GlobalStyles } from './styles/global'
-import { theme } from './styles/theme'
+import { GlobalStyles } from '@styles/global'
+import { theme } from '@styles/theme'
 
 import { App } from './App'
 
