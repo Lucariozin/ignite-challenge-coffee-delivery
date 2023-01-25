d05f458e4babefa3958615881f292b36c2f7a2b5
Lucas Felix <lucariozin.net@gmail.com>
2023-01-25 17:35:33 -0300

    creating the initial styling of the 'presentation container' component.

--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -1,3 +1,88 @@
 import styled from 'styled-components'
 
-export const Container = styled.section``
+export const BlurredBackground = styled.div`
+  height: 34.625rem;
+  position: absolute;
+  right: 0;
+  left: 0;
+
+  background-image: url('/img/blurred-background.svg');
+  background-size: cover;
+  background-position: center;
+
+  filter: blur(2rem);
+`
+
+export const Container = styled.section`
+  padding: 5.875rem 0;
+  position: absolute;
+  z-index: 1;
+`
+
+export const PresentationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  width: 100%;
+  max-width: 35.5rem;
+`
+
+export const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: 800;
+  line-height: 4rem;
+  color: ${({ theme }) => theme.palette.gray[900]};
+`
+
+export const Text = styled.p`
+  font-size: 1.25rem;
+  color: ${({ theme }) => theme.palette.gray[800]};
+`
+
+export const AdvantagesContainer = styled.div`
+  display: flex;
+  gap: 2.5rem;
+  margin-top: 3.125rem;
+`
+
+export const LeftColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+`
+
+export const RightColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+`
+
+export const AdvantageItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+`
+
+interface AdvantageIconProps {
+  backgroundColor: string
+}
+
+export const AdvantageIcon = styled.div<AdvantageIconProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+
+  color: ${({ theme }) => theme.palette.gray[100]};
+  background-color: ${({ backgroundColor }) => backgroundColor};
+`
+
+export const AdvantageText = styled.span`
+  color: ${({ theme }) => theme.palette.gray[700]};
+`
+
+export const CoffeeImage = styled.div``
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,75 @@
-import { Container } from './Home.styles'
+import { useTheme } from 'styled-components'
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+
+import {
+  AdvantageIcon,
+  AdvantageItem,
+  AdvantagesContainer,
+  AdvantageText,
+  BlurredBackground,
+  CoffeeImage,
+  Container,
+  LeftColumn,
+  PresentationContainer,
+  RightColumn,
+  Text,
+  Title,
+} from './Home.styles'
 
 export const Home = () => {
+  const { palette } = useTheme()
+
   return (
-    <Container>
-      <h1>Home</h1>
-    </Container>
+    <>
+      <BlurredBackground />
+
+      <Container>
+        <PresentationContainer>
+          <Title>Encontre o café perfeito para qualquer hora do dia</Title>
+
+          <Text>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</Text>
+
+          <AdvantagesContainer>
+            <LeftColumn>
+              <AdvantageItem>
+                <AdvantageIcon backgroundColor={palette.yellow[500]}>
+                  <ShoppingCart size={16} weight="fill" />
+                </AdvantageIcon>
+
+                <AdvantageText>Compra simples e segura</AdvantageText>
+              </AdvantageItem>
+
+              <AdvantageItem>
+                <AdvantageIcon backgroundColor={palette.yellow[400]}>
+                  <Timer size={16} weight="fill" />
+                </AdvantageIcon>
+
+                <AdvantageText>Entrega rápida e rastreada</AdvantageText>
+              </AdvantageItem>
+            </LeftColumn>
+
+            <RightColumn>
+              <AdvantageItem>
+                <AdvantageIcon backgroundColor={palette.gray[700]}>
+                  <Package size={16} weight="fill" />
+                </AdvantageIcon>
+
+                <AdvantageText>Embalagem mantém o café intacto</AdvantageText>
+              </AdvantageItem>
+
+              <AdvantageItem>
+                <AdvantageIcon backgroundColor={palette.purple[400]}>
+                  <Coffee size={16} weight="fill" />
+                </AdvantageIcon>
+
+                <AdvantageText>O café chega fresquinho até você</AdvantageText>
+              </AdvantageItem>
+            </RightColumn>
+          </AdvantagesContainer>
+        </PresentationContainer>
+
+        <CoffeeImage />
+      </Container>
+    </>
   )
 }
