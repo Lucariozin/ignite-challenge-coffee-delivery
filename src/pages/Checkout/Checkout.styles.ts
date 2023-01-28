28f40ff6485453dad38e62df8e6cd19cb97d051d
Lucas Felix <lucariozin.net@gmail.com>
2023-01-27 22:32:53 -0300

    initial structuring of the checkout page.

---
+++ src/pages/Checkout/Checkout.styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``
+
+export const LeftColumn = styled.section``
+
+export const OrderTitle = styled.h1``
+
+export const DeliveryInformationContainer = styled.div``
+
+export const AddressContainer = styled.div``
+
+export const Icon = styled.div``
+
+export const AddressTextContainer = styled.div``
+
+export const DeliveryAddressTitle = styled.h3``
+
+export const DeliveryAddressText = styled.p``
+
+export const AddressForm = styled.form``
+
+export const Input = styled.input``
+
+export const AddressInputsWrapper = styled.div``
+
+export const HouseNumberAndDistrictContainer = styled.div``
+
+export const AddressComplementContainer = styled.div``
+
+export const CityAndFederativeUnitContainer = styled.div``
+
+export const OrderInformationContainer = styled.main``
+
+export const PaymentInformationContainer = styled.div``
--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,7 +1,62 @@
+import {
+  AddressComplementContainer,
+  AddressContainer,
+  AddressForm,
+  AddressInputsWrapper,
+  AddressTextContainer,
+  CityAndFederativeUnitContainer,
+  Container,
+  DeliveryAddressText,
+  DeliveryAddressTitle,
+  DeliveryInformationContainer,
+  HouseNumberAndDistrictContainer,
+  Icon,
+  Input,
+  LeftColumn,
+  OrderTitle,
+  PaymentInformationContainer,
+} from './Checkout.styles'
+
 export const Checkout = () => {
   return (
-    <>
-      <h1>Checkout</h1>
-    </>
+    <Container>
+      <LeftColumn>
+        <OrderTitle>Complete seu pedido</OrderTitle>
+
+        <DeliveryInformationContainer>
+          <AddressContainer>
+            <Icon />
+
+            <AddressTextContainer>
+              <DeliveryAddressTitle>Endereço de Entrega</DeliveryAddressTitle>
+              <DeliveryAddressText>Informe o endereço onde deseja receber seu pedido</DeliveryAddressText>
+            </AddressTextContainer>
+          </AddressContainer>
+
+          <AddressForm>
+            <Input />
+            <Input />
+
+            <AddressInputsWrapper>
+              <HouseNumberAndDistrictContainer>
+                <Input />
+                <Input />
+              </HouseNumberAndDistrictContainer>
+
+              <AddressComplementContainer>
+                <Input />
+
+                <CityAndFederativeUnitContainer>
+                  <Input />
+                  <Input />
+                </CityAndFederativeUnitContainer>
+              </AddressComplementContainer>
+            </AddressInputsWrapper>
+          </AddressForm>
+        </DeliveryInformationContainer>
+
+        <PaymentInformationContainer></PaymentInformationContainer>
+      </LeftColumn>
+    </Container>
   )
 }
