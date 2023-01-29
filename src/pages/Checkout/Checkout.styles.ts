14d08408163653be64e62e394c6a123aac7ce245
Lucas Felix <lucariozin.net@gmail.com>
2023-01-29 17:09:56 -0300

    styling the 'order summary container' component.

--- src/pages/Checkout/Checkout.styles.ts
+++ src/pages/Checkout/Checkout.styles.ts
@@ -55,7 +55,9 @@ export const CartItem = styled.div`
   gap: 1.25rem;
 `
 
-export const ItemImage = styled.img``
+export const ItemImage = styled.img`
+  user-select: none;
+`
 
 export const ActionsContainer = styled.div`
   display: flex;
@@ -107,18 +109,73 @@ export const ItemPrice = styled.span`
   margin: 0 0 auto auto;
 `
 
-export const OrderSummaryContainer = styled.div``
+export const OrderSummaryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.875rem;
+  margin: 1.5rem 0;
+`
 
-export const SummaryRow = styled.div``
+export const SummaryRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
 
-export const SummaryText = styled.span``
+export const SummaryText = styled.span`
+  font-size: 0.875rem;
+  color: ${({ theme }) => theme.palette.gray[700]};
+`
 
-export const SummaryPrice = styled.span``
+export const SummaryPrice = styled.span`
+  color: ${({ theme }) => theme.palette.gray[700]};
+`
 
-export const SummaryTotalContainer = styled.div``
+export const SummaryTotalContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
 
-export const SummaryTotalText = styled.h3``
+export const SummaryTotalText = styled.h3`
+  font-size: 1.25rem;
+  font-weight: 700;
+  font-family: 'Roboto', sans-serif;
+  color: ${({ theme }) => theme.palette.gray[800]};
+`
 
-export const SummaryTotalPrice = styled.span``
+export const SummaryTotalPrice = styled.span`
+  font-size: 1.25rem;
+  font-weight: 700;
+  font-family: 'Roboto', sans-serif;
+  color: ${({ theme }) => theme.palette.gray[800]};
+`
 
-export const ConfirmOrderButton = styled.button``
+export const ConfirmOrderButton = styled.button`
+  width: 100%;
+  height: 2.875rem;
+
+  color: #ffffff;
+  font-weight: 700;
+  font-size: 0.875rem;
+
+  border: none;
+  border-radius: 6px;
+  background-color: ${({ theme }) => theme.palette.yellow[400]};
+
+  outline: 0;
+  outline-color: transparent;
+  cursor: pointer;
+  user-select: none;
+  transition: background-color 0.2s, outline-color 0.2s;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.yellow[500]};
+  }
+
+  &:focus-visible {
+    background-color: ${({ theme }) => theme.palette.yellow[500]};
+    outline: 1px solid ${({ theme }) => theme.palette.yellow[400]};
+    outline-offset: 2px;
+  }
+`
