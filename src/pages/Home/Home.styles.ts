2b396aeae31361371ec157c4545f903b61c88b2f
Lucas Felix <lucariozin.net@gmail.com>
2023-02-10 20:19:22 -0300

    adding responsiveness on 'home' page.

--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -8,6 +8,14 @@ export const OurCoffeesTitle = styled.h2`
   font-size: 2rem;
   font-weight: 800;
   color: ${({ theme }) => theme.palette.gray[800]};
+
+  ${({ theme }) => theme.breakpoints.down('md')} {
+    font-size: 1.75rem;
+  }
+
+  ${({ theme }) => theme.breakpoints.down(410)} {
+    font-size: 1.25rem;
+  }
 `
 
 export const CoffeeList = styled.div`
@@ -15,4 +23,9 @@ export const CoffeeList = styled.div`
   grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
   gap: 2rem;
   margin-top: 3.375rem;
+
+  ${({ theme }) => theme.breakpoints.down('md')} {
+    gap: 2rem 1rem;
+    grid-template-columns: repeat(auto-fit, minmax(14.375rem, 1fr));
+  }
 `
